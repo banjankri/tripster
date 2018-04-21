8bc45948348854930902766382b9ca8078dc99d7
Artur Banas <banas.artur@gmail.com>
2018-04-21 10:59:07 +0200

    removing redundant function and adding missing import

--- src/app/services/data-sources/plants/gbif.datasource.service.ts
+++ src/app/services/data-sources/plants/gbif.datasource.service.ts
@@ -3,17 +3,12 @@ import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/catch';
+import 'rxjs/observable/throw';
 
 @Injectable()
 export class GbifDataSourceService {
 
     constructor(private http: Http) {
-        // this.getEaxmpleOccurence();
-    }
-
-    getEaxmpleOccurence() {
-        this.http.get(GbifDataSourceService.GBIF_API_URL + `occurrence/search?q=lophophora`)
-            .map(res => res.json()).subscribe();
     }
 
     searchPlantsByQuery(query: string) {
